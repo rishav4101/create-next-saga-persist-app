de8c1aefbf284d2ada94f4dfdd3451a617616541
rishav4101 <rajkumarrishav4101@gmail.com>
2021-06-30 06:56:57 +0530

    [Fix] create script

--- bin/create-app.js
+++ bin/create-app.js
@@ -1,54 +1,64 @@
 #! /usr/bin/env node
 
-"use strict";
+'use strict';
 
-import { execSync } from "child_process";
-import { join } from "path";
-import { mkdirSync, rmdirSync } from "fs";
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+async function runCmd(command) {
+    try {
+      const { stdout, stderr } = await exec(command);
+      console.log(stdout);
+      console.log(stderr);
+    } catch {
+      (error) => {
+        console.log('\x1b[31m', error, '\x1b[0m');
+      };
+    }
+  }
 
 if (process.argv.length < 3) {
-  console.log("\x1b[31m", "You have to provide a name to your app.");
-  console.log("For example :");
-  console.log("    npx next-saga-persist-boilerplate my-app");
-  process.exit(1);
+    console.log('\x1b[31m', 'You have to provide a name to your app.');
+    console.log('For example :');
+    console.log('    npx next-saga-persist-boilerplate my-app');
+    process.exit(1);
 }
 
 const projectName = process.argv[2];
 const currentPath = process.cwd();
-const projectPath = join(currentPath, projectName);
-const git_repo =
-  "https://github.com/rishav4101/next-saga-persist-boilerplate.git";
+const projectPath = path.join(currentPath, projectName);
+const git_repo = "https://github.com/rishav4101/next-saga-persist-boilerplate.git";
 
 try {
-  mkdirSync(projectPath);
-} catch (err) {
-  if (err.code === "EEXIST") {
-    console.log(
-      `The file ${projectName} already exist in the current directory, please give it another name.`
-    );
-  } else {
-    console.log(error);
+    fs.mkdirSync(projectPath);
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
+    } else {
+      console.log(error);
+    }
+    process.exit(1);
   }
-  process.exit(1);
-}
 
-async function main() {
-  try {
-    console.log("Downloading files...");
-    execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
+  async function main() {
+    try {
+      console.log('Downloading files...');
+      execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
 
-    process.chdir(projectPath);
+      process.chdir(projectPath);
 
-    console.log("Installing dependencies...");
-    execSync("npm install");
+      console.log('Installing dependencies...');
+      execSync('npm install');
 
-    console.log("Removing useless files");
-    execSync("npx rimraf ./.git");
-    rmdirSync(join(projectPath, "bin"), { recursive: true });
+      console.log('Removing useless files');
+      execSync('npx rimraf ./.git');
+      fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true});
 
-    console.log("The installation is done, this is ready to use !");
-  } catch (error) {
-    console.log(error);
-  }
+      console.log('The installation is done, this is ready to use !');
+
+    } catch (error) {
+      console.log(error);
+    }
 }
 main();
